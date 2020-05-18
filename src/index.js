683e479e08bddef677c882119053accf9a8de642
Henning Bredel <bredel@52north.org>
2020-05-18 16:08:54 +0200

    Remap entities according to discussed data model

---
+++ src/index.js
@@ -0,0 +1,337 @@
+const fs = require('fs');
+const superagent = require("superagent");
+const parse = require('csv-parse');
+
+// const columns = [
+// "Altitud",
+// "Calle o plaza",
+// "Centre/Entitat",
+// "Codi de l'observador",
+// "Codi del punt d'observació",
+// "Comunidad autónoma",
+// "Enlace a la foto",
+// "Estadio",
+// "Fecha de la identificación (dwc)",
+// "Fenofase",
+// "Hàbitat o ecosistema",
+// "Identificador Gràfic intern",
+// "Identificador d'individu",
+// "Identificador de l'individu observat",
+// "Nom vulgar",
+// "Nombre del paraje",
+// "Si és planta: tipus de dispersió de les llavors",
+// "Si és planta: tipus de fulla",
+// "Zona protegida",
+// "cached_votes_total",
+// "captive",
+// "captive_flag",
+// "comments_count",
+// "community_taxon_id",
+// "coordinates_obscured",
+// "created_at",
+// "created_at_utc",
+// "delta",
+// "description",
+// "faves_count",
+// "geoprivacy",
+// "iconic_taxon_id",
+// "iconic_taxon_name",
+// "id",
+// "id_please",
+// "identif_created_at_0",
+// "identif_created_at_1",
+// "identif_created_at_2",
+// "identif_created_at_3",
+// "identif_created_at_4",
+// "identif_created_at_5",
+// "identif_created_at_6",
+// "identif_current_0",
+// "identif_current_1",
+// "identif_current_2",
+// "identif_current_3",
+// "identif_current_4",
+// "identif_current_5",
+// "identif_current_6",
+// "identif_id_6",
+// "identif_id_0",
+// "identif_id_1",
+// "identif_id_2",
+// "identif_id_3",
+// "identif_id_4",
+// "identif_id_5",
+// "identif_taxon_change_id_2",
+// "identif_taxon_change_id_3",
+// "identif_taxon_name_2",
+// "identif_taxon_name_3",
+// "identif_taxon_rank_2",
+// "identif_taxon_rank_3",
+// "identif_updated_at_2",
+// "identif_updated_at_3",
+// "identif_user_id_2",
+// "identif_user_id_3",
+// "identif_user_login_2",
+// "identif_user_login_3",
+// "identif_user_name_2",
+// "identif_user_name_3",
+// "identifications_count",
+// "last_indexed_at",
+// "latitude",
+// "location_is_exact",
+// "longitude",
+// "map_scale",
+// "mappable",
+// "num_identification_agreements",
+// "num_identification_disagreements",
+// "oauth_application_id",
+// "observation_photos_count",
+// "observation_sounds_count",
+// "observed_on",
+// "observed_on_string",
+// "out_of_range",
+// "owners_identification_from_vision",
+// "photo_attribution_0",
+// "photo_attribution_1",
+// "photo_attribution_2",
+// "photo_attribution_3",
+// "photo_attribution_4",
+// "photo_attribution_5",
+// "photo_attribution_6",
+// "photo_id_0",
+// "photo_id_1",
+// "photo_id_2",
+// "photo_id_3",
+// "photo_id_4",
+// "photo_id_5",
+// "photo_id_6",
+// "photo_large_url_0",
+// "photo_large_url_1",
+// "photo_large_url_2",
+// "photo_large_url_3",
+// "photo_large_url_4",
+// "photo_large_url_5",
+// "photo_large_url_6",
+// "place_guess",
+// "positional_accuracy",
+// "positioning_device",
+// "positioning_method",
+// "project_id_0",
+// "project_id_1",
+// "project_id_2",
+// "project_id_3",
+// "project_title_0",
+// "project_title_1",
+// "project_title_2",
+// "project_title_3",
+// "public_positional_accuracy",
+// "quality_grade",
+// "site_id",
+// "species_guess",
+// "taxon_geoprivacy",
+// "taxon_id",
+// "taxon_name",
+// "taxon_rank",
+// "time_observed_at",
+// "time_observed_at_utc",
+// "time_zone",
+// "updated_at",
+// "updated_at_utc",
+// "uri",
+// "user_id",
+// "user_login",
+// "usuario externo",
+// "uuid",
+// "zic_time_zone"
+// ];
+
+// CONSTANTS
+const natusferaBaseUrl = "https://natusfera.gbif.es";
+const staBaseUrl = "http://localhost:8080/sta";
+const emptyUOM = {
+  name: null,
+  symbol: null,
+  definition: null
+};
+
+
+try {
+
+  // superagent.get("https://external.opengeospatial.org/twiki_public/pub/CitSciIE/OpenDataChallenge/RitmeNatura_odc.csv")
+  //           .set("user-agent", "some-agent")
+  //           .set("accept", "*/*; charset=utf-8")
+  //           .end((err, res) => {
+  //              const data = res.text;
+  const data = fs.readFileSync('./RitmeNatura_odc.csv');
+  
+  const output = [];
+  parse(data, {
+    columns: true,
+    delimiter: ";",
+    trim: true
+  }, function(err, data) {
+    data.forEach(data => {
+      const record = {
+        id: data.id,
+        uri: data.uri,
+        user_id: data.user_id,
+        user_login: data.user_login,
+        observation: {
+          taxon: {
+            observationType: "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CategoryObservation",
+            taxon_name: data.taxon_name,
+            species_guess: data.species_guess,
+            fenofase: data.Fenofase
+          },
+          //},{
+          //  fenofase: {
+          //   observedProperty: "fenofase",
+          //   observationType: "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CategoryObservation",
+          //   phase: data.Fenofase
+          // }
+        },
+        time_observed_at: data.time_observed_at,
+        location: {
+          name: "location of user " + data.user_login,
+          description: "The location where the observation has been made (insitu for now)",
+          encodingType: "application/vnd.geo+json",
+          location: createPoint(data)
+        }
+      };
+
+      record.observation_photos = [];
+      for (let i = 0 ; i < data.observation_photos_count ; i++) {
+        record.observation_photos.push({
+          id: data["photo_id_" + i],
+          attribution: data["photo_attribution_" + i],
+          url: data["photo_large_url_" + i]
+        });
+      }
+
+      if (record.user_id) {
+        output.push(record);
+      }
+    });
+
+  }).on("end", async () => {
+    try {
+
+      // PREPARE CONSTANTS IF NOT AVAILABLE
+
+      // sensor
+      const sensorsResponse = (await superagent.get(staBaseUrl + "/Sensors")).body;
+      const citSciSensor = {
+        name: "Citizen Scientist",
+        description: "Person sharing individual observations to the public for scientific use",
+        encodingType: "application/pdf",
+        metadata: "https://www.weobserve.eu/cops-glossary/#6af6a6f33a552b418"
+      };
+      const sensor = sensorsResponse.value.find(sensor => sensor.name === "Citizen Scientist");
+      const sensorId = sensor ? sensor["@iot.id"] : (await postSensor(citSciSensor)).body["@iot.id"];
+
+      // observed property
+      const observedPropertiesResponse = (await superagent.get(staBaseUrl + "/ObservedProperties")).body;
+      const taxonObservedProperty = {
+        name: "Taxon",
+        description: "The canonical name of the observed species",
+        definition: "http://purl.org/biodiversity/taxon/"
+      };
+      const observedProperty = observedPropertiesResponse.value.find(observedProperty => observedProperty.name === "Taxon");
+      const observedPropertyId = observedProperty ? observedProperty["@iot.id"] : (await postObservedProperty(taxonObservedProperty)).body["@iot.id"];
+
+      // GET KNOWN CONTENT
+      const things = (await superagent.get(staBaseUrl + "/Things")).body;
+      const thingNames = things.value.map(thing => thing.name);
+      const datastreamsResponse = (await superagent.get(staBaseUrl + "/Datastreams")).body;
+
+      const newThings = {};
+      output.forEach(record => {
+        if (!newThings[record.id] && !thingNames.includes(record.user_login)) {
+          const observation = record.observation.taxon;
+          observation.observedPropertyId = observedPropertyId;
+          newThings[record.user_id] = createThing(record, sensorId, observation);
+        }
+      });
+      Object.keys(newThings)
+            .forEach(user_id => postThing(newThings[user_id]));
+
+      // get all things and datastreams already available
+
+      // foreach records
+      // - (optionally) create thing (user) and related data stream
+      // - (optionally) create data stream
+
+      console.log("finished");
+    } catch(err) {
+      console.error(err);
+    }
+  });
+} catch (error) {
+  console.error(error);
+} 
+
+function createPoint(data) {
+  let lon = parseFloat(data.longitude);
+  let lat = parseFloat(data.latitude);
+  if (isNaN(lon) || isNaN(lat)) {
+    //console.warn("invalid location data (lon: " + data.longitude + ", lat: " + data.latitude + ")");
+    lon = 0;
+    lat = 0;
+  }
+  return {
+    type: "Point",
+    coordinates: [lon, lat]
+  }
+}
+
+function createThing(record, sensorId, observation) {
+  const user = record.user_login;
+  const datastreams = createDatastream(user, sensorId, observation);
+  const thing = {
+    name: user,
+    description: "Citizen Scientist",
+    properties: {
+      uri: natusferaBaseUrl + "/users/" + record.user_id
+    },
+    Locations: [
+      record.location
+    ],
+    Datastreams: datastreams
+  };
+  return thing;
+}
+
+function createDatastream(user, sensorId, observation) {
+  return [{
+    name: "Datastream of user " + user + " (observing " + observation.name + ")",
+    description: "An observation datastream taken from a Citizen Scientist via mobile phone",
+    observationType: observation.observationType,
+    ObservedProperty: {
+      "@iot.id": observation.observedPropertyId
+    },
+    unitOfMeasurement: emptyUOM,
+    Sensor: {
+      "@iot.id": sensorId
+    }
+  }];
+}
+
+async function postSensor(sensor) {
+  return sendPost(staBaseUrl + "/Sensors", sensor);
+}
+
+async function postObservedProperty(observedProperty) {
+  return sendPost(staBaseUrl + "/ObservedProperties", observedProperty);
+}
+
+function postThing(thing) {
+  sendPost(staBaseUrl + "/Things", thing);
+}
+
+async function sendPost(url, payload) {
+  return superagent.post(url)
+    .send(payload)
+    .set("content-type", "application/json; charset=utf-8")
+    .set("accept", "application/json")
+    .catch(error => {
+      console.error(JSON.stringify(error.response.body, 2));
+    });
+}
