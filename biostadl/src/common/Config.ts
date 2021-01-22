871ca4d4808932ff13b3695721a3d988244647ba
Henning Bredel <bredel@52north.org>
2021-01-22 16:51:29 +0100

    Clean up httpd

--- biostadl/src/common/Config.ts
+++ biostadl/src/common/Config.ts
@@ -1,3 +1,4 @@
 
-export const staBaseUrl = "http://localhost:8080/sta";
+// export const staBaseUrl = "http://localhost:8080/sta";
 // export const staBaseUrl = "http://cos4cloud.demo.52north.org:8080/sta";
+export const staBaseUrl = "https://cos4cloud.echo.52north.org/sta";
